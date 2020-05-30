79157f66b6a5b5484526b1f600c19c36507ced68
chris <cstradtman@Mac-Mini.local>
2020-05-30 13:12:17 -0400

    modified test script to use __main__

--- test_methods.py
+++ test_methods.py
@@ -4,22 +4,25 @@ __author__ = "Chris Stradtman"
 __license__ = "MIT"
 __version__ = "1.0"
 
+def test_methods():
+    requestlist = [
+        {"repo": "mcr.microsoft.com", "repopath": "dotnet/core/sdk"},
+        {"repo": "index.docker.io", "repopath": "library/hello-world"}
+    ]
 
-requestlist = [
-    {"repo": "mcr.microsoft.com", "repopath": "dotnet/core/sdk"},
-    {"repo": "index.docker.io", "repopath": "library/hello-world"}
-]
+    for request in requestlist:
+        if "username" in request:
+            mysession = repo.RepoSession(
+                request["repo"], request["username"], request["password"])
+        else:
+            mysession = repo.RepoSession(request["repo"])
+        mysession.ConnectToRepository(request["repopath"])
+        taglist = mysession.GetTagList()
+        assert isinstance(taglist, list)
+        manifest = mysession.GetManifest('latest')
+        etag = utils.getManifestEtag(manifest)
+        assert isinstance(etag, str)
 
-for request in requestlist:
-    if "username" in request:
-        mysession = repo.RepoSession(
-            request["repo"], request["username"], request["password"])
-    else:
-        mysession = repo.RepoSession(request["repo"])
-    mysession.ConnectToRepository(request["repopath"])
-    taglist = mysession.GetTagList()
-    assert isinstance(taglist, list)
-    manifest = mysession.GetManifest('latest')
-    etag = utils.getManifestEtag(manifest)
-    assert isinstance(etag, str)
-
+if __name__ == "__main__":
+    # execute only if run as a script
+    test_methods()
